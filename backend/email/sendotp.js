7a986e5f9e6ceba50759a25ff7fa5fa543636371
Jagan0303 <killijagan11@gmail.com>
2026-07-07 22:52:16 +0530

    Revert back changes with resend

--- backend/email/sendotp.js
+++ backend/email/sendotp.js
@@ -1,21 +1,22 @@
-const { Resend } = require("resend");
-const dotEnv = require("dotenv");
-dotEnv.config();
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-// Send an email using Resend
-exports.sendOtpEmail = async (email, otp) => {
-  const { data, error } = await resend.emails.send({
-    from: "OTP Verification <onboarding@resend.dev>",
+const nodemailer=require("nodemailer")
+const dotEnv=require("dotenv")
+dotEnv.config()
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // Use true for port 465, false for port 587
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+// Send an email using async/await
+exports.sendOtpEmail=async(email,otp)=>{
+     await transporter.sendMail({
+    from: `"OTP Verification"<${process.env.EMAIL_USER}>`,
     to: email,
     subject: "Your OTP code",
-    html: `<h2>Your OTP is: ${otp}</h2><p>Valid for 5 minutes</p>`,
+   // text: "Hello world?", // Plain-text version of the message
+    html: `<h2>Your OTP is:${otp}</h2><p>Valid for 5 minutes</p>`, // HTML version of the message
   });
-
-  if (error) {
-    throw new Error(error.message || "Failed to send email via Resend");
-  }
-
-  return data;
-};
+}
