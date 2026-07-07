a6016fe447431e8a844243d1711bb2697650c01b
Jagan0303 <killijagan11@gmail.com>
2026-07-07 22:33:42 +0530

    changed email port to 465

--- backend/email/sendotp.js
+++ backend/email/sendotp.js
@@ -3,8 +3,8 @@ const dotEnv=require("dotenv")
 dotEnv.config()
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // Use true for port 465, false for port 587
+  port: 465,
+  secure: true, // Use true for port 465, false for port 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
